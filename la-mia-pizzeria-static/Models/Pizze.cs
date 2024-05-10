ee32fdbdc3665e153a323a3998305c8719f150a5
DorinVieru <dorino.vieru@gmail.com>
2024-05-10 16:37:36 +0200

    UP: Write construct of Pizze

--- la-mia-pizzeria-static/Models/Pizze.cs
+++ la-mia-pizzeria-static/Models/Pizze.cs
@@ -2,6 +2,19 @@
 {
     public class Pizze
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public float Price { get; set; }
 
+        public Pizze() { }
+
+        public Pizze(string name, string description, string img, float price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Img = img;
+            this.Price = price;
+        }
     }
 }
